e407754ae9d6a4acd7109af5f483660a2ba3d92a
DESKTOP-FOQL2FH\drilo <driloncoc@gmail.com>
2025-06-26 20:04:25 +0200

    changes on app.js

--- auto-text-generator/app.js
+++ auto-text-generator/app.js
@@ -2,9 +2,11 @@ const typedTextSpan = document.querySelector(".typed-text");
 const cursor = document.querySelector(".cursor");
 
 let words = ["Awesome", "Fun", "Cool", "Life", "Famous", "Weird"];
+
 const typingDelay = 200;
 const erasingDelay = 200;
 const newLetterDelay = 2000;
+
 let index = 0;
 let charIndex = 0;
 
