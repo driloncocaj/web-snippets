a4d48bb49ef3781e159aeec89425bd65084cc782
DESKTOP-FOQL2FH\drilo <driloncoc@gmail.com>
2025-07-02 22:20:39 +0200

    Added the fix of random background on start

--- random-background/app.js
+++ random-background/app.js
@@ -13,8 +13,11 @@ function randomColor() {
   return color;
 }
 
+document.body.style.backgroundColor = randomColor();
+
 btn.addEventListener("click", () => {
   document.body.style.backgroundColor = randomColor();
   rgb.textContent = document.body.style.backgroundColor;
   hex.innerHTML = randomColor();
+  // Left to fix, the hex code doesn't correlate to the background color since the function is called independely (however it is written)
 });
