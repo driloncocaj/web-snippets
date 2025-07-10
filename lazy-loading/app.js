79981040843854e1d1e1e7d3e15bd90b72a6f095
DESKTOP-FOQL2FH\drilo <driloncoc@gmail.com>
2025-07-10 21:40:13 +0200

    docs(js): add explanatory comments to improve code readability

--- lazy-loading/app.js
+++ lazy-loading/app.js
@@ -16,6 +16,7 @@ function bluring() {
   background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
+//the function
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
