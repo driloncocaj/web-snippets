eca95e4f6a041c1bee9a90deb468e42a0e86bf49
DESKTOP-FOQL2FH\drilo <driloncoc@gmail.com>
2025-07-29 19:03:56 +0200

    feat: add animation to quote generator

--- random-quote/app.js
+++ random-quote/app.js
@@ -1,4 +1,5 @@
 const getQuoteButton = document.querySelector(".get-quote");
+const quoteContainer = document.querySelector(".quote-container");
 
 function getQuote() {
   fetch("https://dummyjson.com/quotes/random")
@@ -6,6 +7,8 @@ function getQuote() {
     .then((data) => {
       document.querySelector(".quote").textContent = `${data.quote}`;
       document.querySelector(".quote-author").textContent = `${data.author}`;
+      quoteContainer.classList.remove("fade-out");
+      quoteContainer.classList.add("fade-in");
     })
     .catch((error) => {
       document.querySelector(".quote").textContent =
