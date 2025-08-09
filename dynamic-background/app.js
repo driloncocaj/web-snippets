2a745a845c9a78cc2549b3bb92748c3d74345f89
DESKTOP-FOQL2FH\drilo <driloncoc@gmail.com>
2025-08-09 16:18:31 +0200

    chore: add assets

--- dynamic-background/app.js
+++ dynamic-background/app.js
@@ -1,2 +1,27 @@
 const backward = document.getElementById("backward");
 const forward = document.getElementById("forward");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("./assets", (req, res) => {
+  fs.readdir("./assets", (err, files) => {
+    if (err) return res.status(500).send(err);
+    res.json(files.map((file) => `./assets/${file}`));
+  });
+});
+
+app.listen(3000);
+
+fetch("./assets")
+  .then((res) => res.json())
+  .then((images) => {
+    let index = 0;
+    document.body.style.backgroundImage = `url(${images[index]})`;
+    backward.addEventListener("click", () => {
+      index = (index + 1) % images.length;
+      document.body.style.backgroundImage = `url(${images[index]})`;
+    });
+  });
